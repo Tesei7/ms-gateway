0069947b48df352a482ea1dddfa4011a7b9a5294
Ilia Bochkarev <ilia_bochkarev@epam.com>
2018-04-06 20:16:57 +0300

    Initial commit

---
+++ src/main/java/tesei7/ms/gateway/ZuulApplication.java
@@ -0,0 +1,21 @@
+package tesei7.ms.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.cloud.sleuth.Sampler;
+import org.springframework.cloud.sleuth.sampler.AlwaysSampler;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+@EnableZuulProxy
+public class ZuulApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ZuulApplication.class, args);
+    }
+
+    @Bean
+    public Sampler defaultSampler() {
+        return new AlwaysSampler();
+    }
+}
